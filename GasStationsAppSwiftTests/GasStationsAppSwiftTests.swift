4cd8661bf1a7322bcfdf45b1fd42e32295d86078
alfredoisrael-lopez <alfredoisrael.lopez@gmail.com>
2016-03-26 22:27:12 -0600

    Initial Commit

---
+++ GasStationsAppSwiftTests/GasStationsAppSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  GasStationsAppSwiftTests.swift
+//  GasStationsAppSwiftTests
+//
+//  Created by Alfredo Israel López Alcalá on 26/03/2016.
+//  Copyright © 2016 Alfredo Lopez. All rights reserved.
+//
+
+import XCTest
+@testable import GasStationsAppSwift
+
+class GasStationsAppSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
